43515c56d9f100ddc8e9e6b024a3c0d659ac5567
oheyh0719 <oheyh0719@gmail.com>
2026-01-14 16:18:22 +0900

    260114

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@
 const swiper = new Swiper("#main_view", {
     wrapperClass: "slide",
     slideClass: "view",
+    slideActiveClass:'active',
     effect: "fade",
     speed: 800,
+    loop:true,
 
     autoplay: {
-        delay: 2000,
+        delay: 4000,
     },
     pagination: {
         el: ".pager",
