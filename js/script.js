6186fba6abf3948b60e1c18ca7538dfb9250686c
oheyh0719 <oheyh0719@gmail.com>
2026-01-15 16:18:10 +0900

    moomin_end

--- js/script.js
+++ js/script.js
@@ -15,4 +15,19 @@ const swiper = new Swiper("#main_view", {
         el: ".pager",
         bulletActiveClass:"active"
     },
+
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev'
+    },
 });
+
+var menu = $("#gnb > ul").clone(); //대상복제
+
+$("#main_menu").append(menu); //복제된요소가 들어갈자리를 지정해줌
+
+$("#main_menu a").addClass("arrow"); //클래스 부여
+
+var sns = $(".sns").clone();
+
+$("footer").prepend(sns); // 복제된 요소를 앞에 넣음
